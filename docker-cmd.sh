48772482915cbb6a0307a87432616fe4e7184975
stn1y <stanleygfu@gmail.com>
2024-03-25 10:17:42 -0700

    2024-03-25 10:17
    - Think fixed restarting issue
        - Needed to add 'exec' before some gosu commands

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -10,10 +10,10 @@ gosu "$USER" python manage.py shell -c "$USER_EXISTS" && gosu "$USER" python man
 
 if [ "$1" = "--debug" ]; then
   # Django development server
-  gosu "$USER" python manage.py runserver "0.0.0.0:$DJANGO_DEV_SERVER_PORT"
+  exec gosu "$USER" python manage.py runserver "0.0.0.0:$DJANGO_DEV_SERVER_PORT"
 else
   # Gunicorn
-  gosu "$USER" gunicorn "$PROJECT_NAME.wsgi:application" \
+  exec gosu "$USER" gunicorn "$PROJECT_NAME.wsgi:application" \
     --bind "0.0.0.0:$GUNICORN_PORT" \
     --workers "$GUNICORN_WORKERS" \
     --timeout "$GUNICORN_TIMEOUT" \
